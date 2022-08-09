09833d05da58f050289f26cf78998d3a6732a42b
Oleg-Melnikow <oleg.melnikau@gmail.com>
2022-08-09 20:29:58 +0300

    feat: add reverse number

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+    let reverseNumber = 0;
+    let m = Math.abs(n)
+    while(m > 0) {
+      const num = m % 10;
+      m = Math.trunc(m / 10);
+      reverseNumber = reverseNumber*10 + num;
+    }
+    console.log(reverseNumber);
+    return reverseNumber;
 }
